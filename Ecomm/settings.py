8148bbdb7ad0005ce7cc955469d9837180f3d84c
unknown <ashishbhandari380@gmail.com>
2025-07-28 14:46:28 +0545

    responsive footer

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,8 +90,6 @@ DATABASES = {
         'PORT': 27017,
     }
 }
-import os
-from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
